3cfbf0a2a57b151be9155f4ccc1f380a319deaea
Divya Virmani <dvirmani@users.noreply.github.com>
2024-02-22 22:09:21 +0200

    Initial commit

---
+++ M2C3.py
@@ -0,0 +1,38 @@
+# Exercise 1
+
+my_string = "Hello, I am learning Python."
+my_number = 20
+my_list = [1,2,3,4,5]
+my_boolean = True
+
+# Exercise 2
+
+select_string = my_string[:3]
+
+# Exercise 3
+
+select_list = my_list[0]
+
+# Exercise 4
+
+new_number = my_number + 10
+
+# Exercise 5
+
+select_list_last = my_list[-1]
+
+# Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+
+# Exercise 7
+first_word = my_string[:5]
+cap_word = first_word.upper()
+my_new_string = my_string.replace(first_word, cap_word)
+
+# Exercise 8
+my_string_interpolation = f'This is my number: {my_number}'
+
+# Exercise 9
+print('hello world')
+
